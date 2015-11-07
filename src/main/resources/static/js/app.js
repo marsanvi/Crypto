eb10acf8557ca0bfaccc8a5b16bb61599445de7f
martin <marsanvi@gmail.com>
2015-11-07 11:21:29 +0100

    Decode function. Form validation.Separated file for angularjs controllers

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,1 +1,32 @@
-var app= angular.module('crypto',[]);
+angular.module('cryptoApp', []).controller(
+		'CaesarController',
+		[
+				'$http',
+				function($http) {
+					var self = this;
+
+					self.encode = function() {
+						console.log('before call encode');
+						var url = '/caesar/encode?' + 'alphabet='
+								+ self.alphabet + '&text=' + self.text
+								+ '&swift=' + self.swift;
+						$http.get(url).then(function(response) {
+							self.result = response.data;
+						}, function(errResponse) {
+							console.error('Error while trying encode');
+						});
+					};
+
+					self.decode = function() {
+						console.log('before call decode');
+						var url = '/caesar/decode?' + 'alphabet='
+								+ self.alphabet + '&text=' + self.text
+								+ '&swift=' + self.swift;
+						$http.get(url).then(function(response) {
+							self.result = response.data;
+						}, function(errResponse) {
+							console.error('Error while trying decode');
+						});
+					};
+
+				} ]);
--- src/test/java/com/marsanvi/crypto/CaesarMockTest.java
+++ src/test/java/com/marsanvi/crypto/CaesarMockTest.java
@@ -2,14 +2,9 @@ package com.marsanvi.crypto;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
